84372fdf104f8895cc147c748e63caef90650c8b
harshpatil4700 <harshpatil4700@gmail.com>
2025-08-02 15:19:34 +0530

    last commit

--- PRACTICE SET QUESTIONS/PS2Q1.java
+++ PRACTICE SET QUESTIONS/PS2Q1.java
@@ -4,5 +4,6 @@ public class PS2Q1 {
     public static void main(String[] args) {
         float a = 7/4.0f * 9/2.0f;     // in this both * and  / have same precedence so we evaluate from left to right
         System.out.println(a);
+        // System.out.println(a);
     }
 }
